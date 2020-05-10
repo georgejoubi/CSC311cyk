da31b0d3690a75baa4d9a0952756d92095d036cd
georgejoubi <62437699+georgejoubi@users.noreply.github.com>
2020-05-10 20:53:22 +0300

    merge & cartesian sloved

--- utilities.h
+++ utilities.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <tuple>
 
 using namespace std;
 
@@ -34,11 +35,40 @@ public:
    doesn't look good either
    */
 template <typename T>
-set<T>& merge(set<T>& ls,set<T>& rs);
+set<T>& merge(set<T>& ls, set<T>& rs) {
+
+	std::set<T> s;
+
+	std::set_union(ls.begin(), ls.end(), rs.begin(), rs.end(), std::inserter(s, s.begin()));
+
+	return s;
+
+}
 
 /* returns the cartesian 
 	product of two sets
 	*/
 template <typename T>
-set<T> cartesian(set<T>& s1,
-	set<T>& s2);
+set<T> cartesian(set<T>& s1, set<T>& s2) {
+
+	set<T> result;
+
+	int n1 = s1.size();
+	int n2 = s2.size();
+	
+	std::tuple<T><T> current;
+	current = make_tuple(s1.begin(), s2.begin());
+
+	for (std::set<T>::iterator it1 = s1.begin(); it1 != s1.end(); it1++) {
+		for (std::set<T>::iteratorvit2 = s2.begin(); it2 != s2.end(); it2++) {
+
+			get<0>(current) = *it1;
+			get<1>(current) = *it2;
+			result.insert(current);
+
+		}
+	}
+
+	return result;
+
+}
