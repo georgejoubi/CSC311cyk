0b1e109d5713e58ed99e92cdb9b563f8c6e65bd1
georgejoubi <62437699+georgejoubi@users.noreply.github.com>
2020-05-11 23:10:24 +0300

    solved search
    still working on void solve(Matrix<set<string>>& m, string input)

--- main.cpp
+++ main.cpp
@@ -8,28 +8,43 @@
 	  it does not insert an element that
 	  is already in the set
    2. If you have a c++17 conformant compiler
-      you don't have to implement the merge 
+	  you don't have to implement the merge
 	  function, you can use the set.merge(...)
-   3. Test the smaller functions first 
+   3. Test the smaller functions first
 	  before attempting to implement solve
    4. Try designing solve on a piece of paper
-      before coding
+	  before coding
 */
 using namespace std;
-vector < pair<string, string>> productions;
+vector<pair<string, string>> productions;
 
 /* given a symbol  "symbol"
 	returns a set of all symbols
 	that can produce it
 	*/
-set<string> search(string symbol);
+set<string> search(string symbol) {
+	set<string> result;
+	for (std::vector<int>::iterator it = productions.begin(); it != productions.end(); ++it) {
+		if (*it.second == symbol) {
+			result.insert(*it.first);
+		}
+	}
+	return result;
+}
 
 
 /* generalization of the above
    returns a set of symbols that
    can produce a set of symbols
    */
-set<string> search(set<string> ss);
+set<string> search(set<string> ss) {
+	set<string> result;
+	for (std::set<int>::iterator it = ss.begin(); it != ss.end(); ++it) {
+		set<string> current = search(*it);
+		result = merge(result, current);
+	}
+	return result;
+}
 
 /* this is the heart of
    the CYK algorithm
@@ -37,18 +52,42 @@ set<string> search(set<string> ss);
    and string imput and "fills"
    the solution
    */
-void solve(Matrix<set<string>>& m, string input);
+void solve(Matrix<set<string>>& m, string input) {
+
+	int n = input.length();
+
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= n - i; j++) {
+			
+			string target = input.substr(i, i + j + 1);
+			set<string> ss;
+			
+			if (target.length() == 1) {
+				m[i][j] = search(target);
+			} else {
+
+				for (int x = 1; x < target.length() - 1; x++) {
+					string left = target.substr(0, x);
+					string right = target.substr(x, target.length());
+					ss.merge(ss,cartesian(m[i][j-x]))
+				}
+
+			}
+		}
+	}
+
+}
 
 int main() {
 	cout << "input a string\n";
 	string input;
 	cin >> input;
-	/* Assumes that the starting 
+	/* Assumes that the starting
 		symbol is "S"
 		*/
-	Matrix<set<string>> table(input.length(), input.length()) ;
+	Matrix<set<string>> table(input.length(), input.length());
 	std::ifstream file;
-	/* read the cfg from file "cfg.txt" 
+	/* read the cfg from file "cfg.txt"
 	   if the grammar is CNF*/
 	file.open("cfg.txt");
 	int np;
